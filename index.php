92498197e8feced54fb48832fa6951dac8ae9480
lalit <lalit.kumawat@synechron.com>
2013-05-10 18:17:49 +0530

    started the coading part with Blank HTML page.

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
-<?php
-
-
-echo "lalit's page";
-?>
+<html>
+<head>
+    <meta content="text/html; charset=UTF-8" http-equiv="content-type">
+    <title>Lalit's App</title>
+    <link href="css/style.css" media="screen" rel="stylesheet" type="text/css">
+    <script src="scripts/jquery.js" type="text/javascript"></script><style></style>
+  </head>
+  <body class="app-wrapper">
+    <div class="content">
+      <div class="notification"></div>
+      <div class="message">
+        <h1>Lalit's App</h1>
+      </div>
+      
+      <div class="pics">
+       
+      </div>
+    </div>
+</body></html>
